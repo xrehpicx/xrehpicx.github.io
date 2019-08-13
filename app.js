c0a2987c89e4b76f559389a808d8dec571f04a14
xrehpicx <xrehpicx@gmail.com>
2019-08-13 19:33:20 +0530

    js

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+function show() {
+    var eleList = ['.place1', '.place2', '.place3'];
 
+    eleList.forEach(function (cl) {
+        var toshow = document.querySelector(cl);
+        var topPosition = toshow.getBoundingClientRect().top;
+        var screenPosition = window.innerHeight/2;
+        if (topPosition < screenPosition) {
+            toshow.style.opacity = 1;
+            toshow.style.transform = "translateY(0)";
+        }else{
+            toshow.style.opacity = 0;
+            toshow.style.transform = "translateY(25)";
+        }
+    });
+}
+
+window.addEventListener('scroll', show);
