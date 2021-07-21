87517d6853e1861890ac166ae2ab67a58ada8a85
Raj Sharma <xrehpicx@gmail.com>
2021-07-21 18:26:49 +0530

    updated desc stuff deploy

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>test</p>
+        <p>portfolio still under development</p>
         <a
           className="App-link"
           href="https://instagram.com/xrehpicx"
