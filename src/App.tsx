56d7d9fa86c4f14f14771d474d2a1c8214e9afd3
Raj Sharma <xrehpicx@gmail.com>
2021-07-21 18:22:26 +0530

    test

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>portfolio still under development</p>
+        <p>test</p>
         <a
           className="App-link"
           href="https://instagram.com/xrehpicx"
